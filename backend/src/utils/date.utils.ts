f10806ca161f2bb4253da4563953a966e6a17554
cachi1997 <cachigaliano1997@gmail.com>
2025-08-06 17:13:30 -0300

    added systeminformation package and date-fns

--- backend/src/utils/date.utils.ts
+++ backend/src/utils/date.utils.ts
@@ -1,3 +1,41 @@
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
+import { startOfWeek, endOfWeek } from "date-fns";
+
+export function getCurrentWeekRangeString(): string {
+  const now = new Date();
+  const start = startOfWeek(now, { weekStartsOn: 1 }); // lunes
+  const end = endOfWeek(now, { weekStartsOn: 1 }); // domingo
+
+  const formatDayMonth = (date: Date) => format(date, "d/M", { locale: es });
+
+  return `${formatDayMonth(start)} - ${formatDayMonth(end)}`;
+}
+
+export function getCurrentMonthName(): string {
+  const now = new Date();
+  const monthNumber = now.getMonth(); // 0 = enero
+  const months = [
+    "enero",
+    "febrero",
+    "marzo",
+    "abril",
+    "mayo",
+    "junio",
+    "julio",
+    "agosto",
+    "septiembre",
+    "octubre",
+    "noviembre",
+    "diciembre",
+  ];
+  return months[monthNumber];
+}
+
+export function getCurrentYear(): number {
+  return new Date().getFullYear();
+}
+
 export const getTodayCutoffDate = (): Date => {
   const now = new Date();
   const cutoff = new Date(
