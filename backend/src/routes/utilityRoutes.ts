80ce84b23dbf7afc7c4b883f6f97b130d7f83023
cachi1997 <cachigaliano1997@gmail.com>
2025-08-19 08:53:13 -0300

    added creation routes

--- backend/src/routes/utilityRoutes.ts
+++ backend/src/routes/utilityRoutes.ts
@@ -1,24 +1,45 @@
 import { Router } from "express";
-import { getClients } from "../controllers/clientController";
-import { getColors } from "../controllers/colorController";
-import { getDeniers } from "../controllers/denierController";
-import { getProducts } from "../controllers/productController";
-import { getRawMaterials } from "../controllers/rawMaterialController";
-import { getTones } from "../controllers/toneController";
+import { createClient, getClients } from "../controllers/clientController";
+import { createColor, getColors } from "../controllers/colorController";
+import { createDenier, getDeniers } from "../controllers/denierController";
+import { createProduct, getProducts } from "../controllers/productController";
+import {
+  createRawMaterial,
+  getRawMaterials,
+} from "../controllers/rawMaterialController";
+import { createTone, getTones } from "../controllers/toneController";
 import { getLastPPE } from "../controllers/ppeController";
 import { getDashboardData } from "../controllers/dashboardController";
-import { getAllSections } from "../controllers/sectionController";
+import {
+  createSection,
+  getAllSections,
+} from "../controllers/sectionController";
 
 const utilityRouter = Router();
 
 utilityRouter.get("/dashboard/summary", getDashboardData);
+
 utilityRouter.get("/clients", getClients);
+utilityRouter.post("/clients", createClient);
+
 utilityRouter.get("/colors", getColors);
+utilityRouter.post("/colors", createColor);
+
 utilityRouter.get("/deniers", getDeniers);
+utilityRouter.post("/deniers", createDenier);
+
 utilityRouter.get("/products", getProducts);
+utilityRouter.post("/products", createProduct);
+
 utilityRouter.get("/rawMaterials", getRawMaterials);
+utilityRouter.post("/rawMaterials", createRawMaterial);
+
 utilityRouter.get("/tones", getTones);
+utilityRouter.post("/tones", createTone);
+
 utilityRouter.get("/lastPPE", getLastPPE);
+
 utilityRouter.get("/sections", getAllSections);
+utilityRouter.post("/sections", createSection);
 
 export default utilityRouter;
