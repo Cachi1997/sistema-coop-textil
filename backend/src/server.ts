c962c82a6eac5243dcd0a808c8e6aa0ad7c871a2
cachi1997 <cachigaliano1997@gmail.com>
2025-09-03 20:43:33 -0300

    added cors configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,13 @@ import deliveryNoteRouter from "./routes/deliveryNoteRoutes";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 app.use("/api/users", userRouter);
